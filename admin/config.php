d3ed8d81991d34d635a3179d9f6e814eb20c1d44
Sagar <sagar.bo@endurance.com>
2016-07-21 22:04:58 +0530

    Modification

--- admin/config.php
+++ admin/config.php
@@ -8,24 +8,26 @@ define('HTTPS_SERVER', 'http://belagam.local/admin/');
 define('HTTPS_CATALOG', 'http://belagam.local/');
 
 // DIR
-define('DIR_APPLICATION', '/Applications/MAMP/htdocs/belagam/admin/');
-define('DIR_SYSTEM', '/Applications/MAMP/htdocs/belagam/system/');
-define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/belagam/admin/language/');
-define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/belagam/admin/view/template/');
-define('DIR_CONFIG', '/Applications/MAMP/htdocs/belagam/system/config/');
-define('DIR_IMAGE', '/Applications/MAMP/htdocs/belagam/image/');
-define('DIR_CACHE', '/Applications/MAMP/htdocs/belagam/system/cache/');
-define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/belagam/system/download/');
-define('DIR_UPLOAD', '/Applications/MAMP/htdocs/belagam/system/upload/');
-define('DIR_LOGS', '/Applications/MAMP/htdocs/belagam/system/logs/');
-define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/belagam/system/modification/');
-define('DIR_CATALOG', '/Applications/MAMP/htdocs/belagam/catalog/');
+
+define('DIR_ROOT', '/Applications/MAMP/htdocs/belagam/');
+define('DIR_APPLICATION', DIR_ROOT.'admin/');
+define('DIR_SYSTEM', DIR_ROOT.'system/');
+define('DIR_LANGUAGE', DIR_ROOT.'admin/language/');
+define('DIR_TEMPLATE', DIR_ROOT.'admin/view/template/');
+define('DIR_CONFIG', DIR_ROOT.'system/config/');
+define('DIR_IMAGE', DIR_ROOT.'image/');
+define('DIR_CACHE', DIR_ROOT.'system/cache/');
+define('DIR_DOWNLOAD', DIR_ROOT.'system/download/');
+define('DIR_UPLOAD', DIR_ROOT.'system/upload/');
+define('DIR_LOGS', DIR_ROOT.'system/logs/');
+define('DIR_MODIFICATION', DIR_ROOT.'system/modification/');
+define('DIR_CATALOG', DIR_ROOT.'catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'belagam_old');
+define('DB_PASSWORD', 'Zerocool1$');
+define('DB_DATABASE', 'belagam');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
--- config.php
+++ config.php
@@ -6,23 +6,24 @@ define('HTTP_SERVER', 'http://belagam.local/');
 define('HTTPS_SERVER', 'http://belagam.local/');
 
 // DIR
-define('DIR_APPLICATION', '/Applications/MAMP/htdocs/belagam/catalog/');
-define('DIR_SYSTEM', '/Applications/MAMP/htdocs/belagam/system/');
-define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/belagam/catalog/language/');
-define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/belagam/catalog/view/theme/');
-define('DIR_CONFIG', '/Applications/MAMP/htdocs/belagam/system/config/');
-define('DIR_IMAGE', '/Applications/MAMP/htdocs/belagam/image/');
-define('DIR_CACHE', '/Applications/MAMP/htdocs/belagam/system/cache/');
-define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/belagam/system/download/');
-define('DIR_UPLOAD', '/Applications/MAMP/htdocs/belagam/system/upload/');
-define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/belagam/system/modification/');
-define('DIR_LOGS', '/Applications/MAMP/htdocs/belagam/system/logs/');
+define('DIR_ROOT', '/Applications/MAMP/htdocs/belagam/');
+define('DIR_APPLICATION', DIR_ROOT.'catalog/');
+define('DIR_SYSTEM', DIR_ROOT.'system/');
+define('DIR_LANGUAGE', DIR_ROOT.'catalog/language/');
+define('DIR_TEMPLATE', DIR_ROOT.'catalog/view/theme/');
+define('DIR_CONFIG', DIR_ROOT.'system/config/');
+define('DIR_IMAGE', DIR_ROOT.'image/');
+define('DIR_CACHE', DIR_ROOT.'system/cache/');
+define('DIR_DOWNLOAD', DIR_ROOT.'system/download/');
+define('DIR_UPLOAD', DIR_ROOT.'system/upload/');
+define('DIR_MODIFICATION', DIR_ROOT.'system/modification/');
+define('DIR_LOGS', DIR_ROOT.'system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_DATABASE', 'belagam_old');
+define('DB_PASSWORD', 'Zerocool1$');
+define('DB_DATABASE', 'belagam');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
